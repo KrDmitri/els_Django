21af076eac58f10e910bf626607f5e41146020b0
bombom cho <drjreric@gmail.com>
2024-06-19 15:53:48 +0900

    mysql 연동되는지만 확인

--- ELS_Eval/settings.py
+++ ELS_Eval/settings.py
@@ -112,8 +112,12 @@ WSGI_APPLICATION = 'ELS_Eval.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'els_db',
+        'USER': 'admin',
+        'PASSWORD': 'qweqweqwe',
+        'HOST': 'els-db.ch3mhm9idfzq.us-east-1.rds.amazonaws.com',
+        'PORT': 3306,
     }
 }
 
